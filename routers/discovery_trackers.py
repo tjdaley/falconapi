2e5d3f062ae20040142dcec9b7a7bc37acc15b96
Tom Daley <tjd@powerdaley.com>
2022-07-19 00:56:03 -0500

    reorg and automate testing

---
+++ routers/discovery_trackers.py
@@ -0,0 +1,85 @@
+"""
+discovery_trackers.py - Falcon API Routers for Discovery Trackers
+"""
+from fastapi import APIRouter, HTTPException, status
+from typing import Dict, List
+from models.document import Document
+from models.response import Response
+from models.tracker import Tracker
+
+router = APIRouter(
+    tags=["Discovery Trackers"],
+    prefix="/trackers",
+    responses={404: {"description": "Not found"}}
+)
+
+trackers: Dict[str, Tracker] = {}
+
+# Add a tracker
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=Response, summary='Add a tracker')
+async def create_tracker(tracker: Tracker):
+    if tracker.id in trackers:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Tracker already exists: {tracker.id}")
+    trackers[tracker.id] = tracker
+    return {'message': "Tracker created"}
+
+# Get a tracker by Tracker ID
+@router.get('/{tracker_id}', status_code=status.HTTP_200_OK, response_model=Tracker, summary='Get a tracker by Tracker ID')
+async def get_tracker(tracker_id: str):
+    if tracker_id not in trackers:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Tracker not found: {tracker_id}")
+    return trackers[tracker_id]
+
+# Get all trackers for a user
+@router.get('/user/{user_name}', status_code=status.HTTP_200_OK, response_model=List[Tracker], summary='Get all trackers for a user')
+async def get_trackers_for_user(user_name: str):
+    return [tracker for tracker in trackers.values() if tracker.user_name == user_name]
+
+# Delete a tracker
+@router.delete('/{tracker_id}', status_code=status.HTTP_200_OK, response_model=Response, summary='Delete a tracker')
+async def delete_tracker(tracker_id: str):
+    if tracker_id not in trackers:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Tracker not found: {tracker_id}")
+    del trackers[tracker_id]
+    return {'message': "Tracker deleted"}
+
+# Add a document to a tracker
+@router.post('/{tracker_id}/documents', status_code=status.HTTP_201_CREATED, response_model=Response, summary='Add a document to a tracker')
+async def add_document_to_tracker(document: Document):
+    if document.tracker_id not in trackers:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Tracker not found:")
+    if document.id not in trackers[document.tracker_id].documents:
+        trackers[document.tracker_id].documents[document.id] = document
+        return {'message': "Document added"}
+    raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Document already exists: {document.path}")
+
+# Get a document from a tracker
+@router.get('/{tracker_id}/documents/{document_id}', status_code=status.HTTP_200_OK, response_model=Document, summary='Get a document from a tracker')
+async def get_document(tracker_id: str, document_id: str):
+    tracker = trackers.get(tracker_id, None)
+    if tracker is None:
+        raise HTTPException(status_code=404, detail=f"Tracker not found: {tracker_id}")
+    doc = tracker.documents.get(document_id, None)
+    if doc is None:
+        raise HTTPException(status_code=404, detail=f"Document not found: {id}")
+    return doc
+
+# Get all documents from a tracker
+@router.get('/{tracker_id}/documents', status_code=status.HTTP_200_OK, response_model=Dict[str, Document], summary='Get all documents from a tracker')
+async def get_documents(tracker_id: str):
+    tracker = trackers.get(tracker_id, None)
+    if tracker is None:
+        raise HTTPException(status_code=404, detail=f"Tracker not found: {tracker_id}")
+    return tracker.documents
+
+# Delete a document from a tracker
+@router.delete('/{tracker_id}/documents/{document_id}', status_code=status.HTTP_200_OK, response_model=Response, summary='Delete a document from a tracker')
+async def delete_document(tracker_id: str, document_id: str):
+    tracker = trackers.get(tracker_id, None)
+    if tracker is None:
+        raise HTTPException(status_code=404, detail=f"Tracker not found: {tracker_id}")
+    doc = tracker.documents.get(document_id, None)
+    if doc is None:
+        raise HTTPException(status_code=404, detail=f"Document not found: {document_id}")
+    del tracker.documents[document_id]
+    return {'message': "Document deleted"}
---
+++ tests/test_discovery_trackers.py
@@ -0,0 +1,142 @@
+"""
+test_root.py - Test the root endpoint
+"""
+import requests
+
+
+SERVER = "http://localhost:8000"
+PREFIX = "/api/v1"
+
+
+def test_root():
+    response = requests.get(SERVER + '/')
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Falcon API Copyright (c) 2022, Thomas J. Daley, Esq. - Version 1.0.0'}
+
+def test_add_tracker():
+    response = requests.post(SERVER + PREFIX + '/trackers', json={'id': '123', 'user_name': 'test', 'name': 'Test Tracker'})
+    assert response.status_code == 201
+    assert response.json() == {'message': 'Tracker created'}
+
+def test_add_duplicate_tracker():
+    response = requests.post(SERVER + PREFIX + '/trackers', json={'id': '124', 'user_name': 'test', 'name': 'Test Tracker'})
+    assert response.status_code == 201
+    assert response.json() == {'message': 'Tracker created'}
+    response = requests.post(SERVER + PREFIX + '/trackers', json={'id': '124', 'user_name': 'test', 'name': 'Test Tracker'})
+    assert response.status_code == 409
+    assert response.json() == {'detail': 'Tracker already exists: 124'}
+
+def test_get_tracker():
+    response = requests.get(SERVER + PREFIX + '/trackers/123')
+    assert response.status_code == 200
+    assert response.json() == {
+        "id": "123",
+        "name": "Test Tracker",
+        "user_name": "test",
+        "documents": {}
+    }
+
+def test_get_tracker_not_found():
+    response = requests.get(SERVER + PREFIX + '/trackers/999')
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Tracker not found: 999'}
+
+def test_get_trackers_for_user():
+    response = requests.get(SERVER + PREFIX + '/trackers/user/test')
+    assert response.status_code == 200
+    assert response.json() == [
+        {
+            "id": "123",
+            "name": "Test Tracker",
+            "user_name": "test",
+            "documents": {}
+        },
+        {
+            "id": "124",
+            "name": "Test Tracker",
+            "user_name": "test",
+            "documents": {}
+        }
+    ]
+
+def test_delete_tracker():
+    response = requests.delete(SERVER + PREFIX + '/trackers/124')
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Tracker deleted'}
+
+def test_delete_tracker_not_found():
+    response = requests.delete(SERVER + PREFIX + '/trackers/999')
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Tracker not found: 999'}
+
+def test_add_document():
+    doc = {
+        "id": "doc-1",
+        "tracker_id":"123",
+        "path":"x:\\shared\\plano\\tjd\\open\\farrar\\discovery\\our production\\2022-07-15 BOA 2304.pdf",
+        "filename":"2022-07-15 BOA 2304.pdf",
+        "title": "BOA 2304",
+        "create_date":"07/01/2022"   
+}
+    response = requests.post(SERVER + PREFIX + '/trackers/123/documents', json=doc)
+    assert response.status_code == 201
+    assert response.json() == {'message': 'Document added'}
+
+def test_add_duplicate_document():
+    path = "x:\\shared\\plano\\tjd\\open\\farrar\\discovery\\our production\\2022-07-15 BOA 2304.pdf"
+    doc = {
+        "id": "doc-2",
+        "tracker_id": "123",
+        "path": path,
+        "filename": "2022-07-15 BOA 2304.pdf",
+        "title": "BOA 2304",
+        "create_date": "07/01/2022"   
+    }
+    response = requests.post(SERVER + PREFIX + '/trackers/123/documents', json=doc)
+    assert response.status_code == 201
+    assert response.json() == {'message': 'Document added'}
+    response = requests.post(SERVER + PREFIX + '/trackers/123/documents', json=doc)
+    assert response.status_code == 409
+    assert response.json() == {'detail': f'Document already exists: {path}'}
+
+def test_get_documents():
+    response = requests.get(SERVER + PREFIX + '/trackers/123/documents')
+    assert response.status_code == 200
+    assert response.json() == {
+        "doc-1": {
+            "id": "doc-1",
+            "tracker_id": "123",
+            "path": "x:\\shared\\plano\\tjd\\open\\farrar\\discovery\\our production\\2022-07-15 BOA 2304.pdf",
+            "filename": "2022-07-15 BOA 2304.pdf",
+            "title": "BOA 2304",
+            "create_date": "07/01/2022",
+            "document_date": None,
+            "beginning_bates": None,
+            "ending_bates": None,
+            "page_count": None,
+            "bates_pattern": None
+        },
+        "doc-2": {
+            "id": "doc-2",
+            "tracker_id": "123",
+            "path": "x:\\shared\\plano\\tjd\\open\\farrar\\discovery\\our production\\2022-07-15 BOA 2304.pdf",
+            "filename": "2022-07-15 BOA 2304.pdf",
+            "title": "BOA 2304",
+            "create_date": "07/01/2022",
+            "document_date": None,
+            "beginning_bates": None,
+            "ending_bates": None,
+            "page_count": None,
+            "bates_pattern": None
+        }
+    }
+
+def test_delete_document():
+    response = requests.delete(SERVER + PREFIX + '/trackers/123/documents/doc-1')
+    assert response.status_code == 200
+    assert response.json() == {'message': 'Document deleted'}
+
+def test_delete_nonexistent_document():
+    response = requests.delete(SERVER + PREFIX + '/trackers/123/documents/doc-999')
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'Document not found: doc-999'}
