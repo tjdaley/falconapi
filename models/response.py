c6a82bb601cfaeed547fb89458972e1bff877e0e
Tom Daley <tjd@powerdaley.com>
2022-07-19 22:49:29 -0500

    add ResponseAndId class

--- models/response.py
+++ models/response.py
@@ -1,12 +1,13 @@
 """
 response.py - Falcon API response
 """
+from typing import Optional
 from pydantic import BaseModel
 
 
 class Response(BaseModel):
     """
-    Response model
+    Response model - For Responses that do NOT contain an ID field.
     """
     message: str
 
@@ -15,5 +16,21 @@ class Response(BaseModel):
         schema_extra = {
             "example": {
                 "message": "Falcon API Copyright (c) 2022, Thomas J. Daley, Esq. - Version 1.0.0"
+            }
+        }
+
+class ResponseAndId(BaseModel):
+    """
+    Response model - For Responses that contain an ID field.
+    """
+    message: str
+    id: Optional[str]
+
+    class Config:
+        orm_mode = True
+        schema_extra = {
+            "example": {
+                "message": "Document added",
+                "id": "2876ce60-0f93-4548-8c2f-ac1014dd8697"
             }
         }
