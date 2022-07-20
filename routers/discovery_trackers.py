d34b141f9088e8ec620a1ffbd53d19be614ff09a
Tom Daley <tjd@powerdaley.com>
2022-07-19 22:50:00 -0500

    update responses to break out 'id' field

--- routers/discovery_trackers.py
+++ routers/discovery_trackers.py
@@ -4,7 +4,7 @@ discovery_trackers.py - Falcon API Routers for Discovery Trackers
 from fastapi import APIRouter, HTTPException, status
 from typing import Dict, List
 from models.document import Document
-from models.response import Response
+from models.response import Response, ResponseAndId
 from models.tracker import Tracker
 
 router = APIRouter(
@@ -16,12 +16,12 @@ router = APIRouter(
 trackers: Dict[str, Tracker] = {}
 
 # Add a tracker
-@router.post('/', status_code=status.HTTP_201_CREATED, response_model=Response, summary='Add a tracker')
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=ResponseAndId, summary='Add a tracker')
 async def create_tracker(tracker: Tracker):
     if tracker.id in trackers:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Tracker already exists: {tracker.id}")
     trackers[tracker.id] = tracker
-    return {'message': "Tracker created"}
+    return {'message': "Tracker created", 'id': tracker.id}
 
 # Get a tracker by Tracker ID
 @router.get('/{tracker_id}', status_code=status.HTTP_200_OK, response_model=Tracker, summary='Get a tracker by Tracker ID')
@@ -36,21 +36,21 @@ async def get_trackers_for_user(user_name: str):
     return [tracker for tracker in trackers.values() if tracker.user_name == user_name]
 
 # Delete a tracker
-@router.delete('/{tracker_id}', status_code=status.HTTP_200_OK, response_model=Response, summary='Delete a tracker')
+@router.delete('/{tracker_id}', status_code=status.HTTP_200_OK, response_model=ResponseAndId, summary='Delete a tracker')
 async def delete_tracker(tracker_id: str):
     if tracker_id not in trackers:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Tracker not found: {tracker_id}")
     del trackers[tracker_id]
-    return {'message': "Tracker deleted"}
+    return {'message': "Tracker deleted", 'id': tracker_id}
 
 # Add a document to a tracker
-@router.post('/{tracker_id}/documents', status_code=status.HTTP_201_CREATED, response_model=Response, summary='Add a document to a tracker')
+@router.post('/{tracker_id}/documents', status_code=status.HTTP_201_CREATED, response_model=ResponseAndId, summary='Add a document to a tracker')
 async def add_document_to_tracker(document: Document):
     if document.tracker_id not in trackers:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Tracker not found:")
     if document.id not in trackers[document.tracker_id].documents:
         trackers[document.tracker_id].documents[document.id] = document
-        return {'message': "Document added"}
+        return {'message': "Document added", 'id': document.id}
     raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Document already exists: {document.path}")
 
 # Get a document from a tracker
@@ -73,7 +73,7 @@ async def get_documents(tracker_id: str):
     return tracker.documents
 
 # Delete a document from a tracker
-@router.delete('/{tracker_id}/documents/{document_id}', status_code=status.HTTP_200_OK, response_model=Response, summary='Delete a document from a tracker')
+@router.delete('/{tracker_id}/documents/{document_id}', status_code=status.HTTP_200_OK, response_model=ResponseAndId, summary='Delete a document from a tracker')
 async def delete_document(tracker_id: str, document_id: str):
     tracker = trackers.get(tracker_id, None)
     if tracker is None:
@@ -82,4 +82,4 @@ async def delete_document(tracker_id: str, document_id: str):
     if doc is None:
         raise HTTPException(status_code=404, detail=f"Document not found: {document_id}")
     del tracker.documents[document_id]
-    return {'message': "Document deleted"}
+    return {'message': "Document deleted", 'id': document_id}
