56a9499965ffc1095848898e4e0aa2e5674d789f
Tom Daley <tjd@powerdaley.com>
2023-12-14 22:27:47 -0600

    provide default values

--- models/realproperty.py
+++ models/realproperty.py
@@ -2,7 +2,7 @@
 realproperty.py - Models for a real property information request and response
 """
 
-from pydantic import BaseModel, Field, validator
+from pydantic import BaseModel, Field
 
 
 class RealPropertyInfoRequest(BaseModel):
@@ -24,7 +24,7 @@ class RealPropertyInfoResponse(BaseModel):
 	tax_year: str = Field(..., example="2022")
 	tax_amount: float = Field(..., example=7016.58)
 	owners: str = Field(..., example="THOMAS J DALEY, AVA P DALEY")
-	approximate_value_midpoint: int = Field(..., example=515908)
-	approximate_value_high: int = Field(..., example=521067)
-	approximate_value_low: int = Field(..., example=510748)
-	equity_amount: int = Field(..., example=445790)
+	approximate_value_midpoint: int = Field(..., example=515908, default=0)
+	approximate_value_high: int = Field(..., example=521067, default=0)
+	approximate_value_low: int = Field(..., example=510748, default=0)
+	equity_amount: int = Field(..., example=445790, default=0)
