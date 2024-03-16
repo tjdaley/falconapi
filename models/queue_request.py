bd932d7309207ac227e50328420cb62baf6208d5
Tom Daley <tjd@powerdaley.com>
2024-03-16 17:12:47 -0500

    add queue path

---
+++ models/queue_request.py
@@ -0,0 +1,65 @@
+"""
+queue_request.py - Pydantic model for QueueRequest
+"""
+
+from typing import Optional, Dict
+from uuid import uuid4
+from pydantic import BaseModel, Field, validator
+
+
+class QueueRequest(BaseModel):
+    request_id: Optional[str] = str(uuid4())
+    task: str
+    payload: Dict
+    username: str
+    ttl: Optional[int] = 4
+
+    class Config:
+        orm_mode = True
+        schema_extra = {
+            "example": {
+                "request_id": "gjdf894nfkbjf8943hjn:2024040100000000",
+                "task": "classify",
+                "payload": {
+                    "doc_id": "doc-1",
+                    "model": "default",
+                },
+                "username": "user1@test.com",
+                "ttl": 4
+            }
+        }
+
+    # validators
+    @validator('task')
+    def task_must_be_in_list(cls, v):
+        valid_tasks = ['classify', 'summarize', 'transactions', 'stop']
+        if v not in valid_tasks:
+            raise ValueError(f'Task must be one of {valid_tasks}')
+        return v
+    
+    @validator('payload')
+    def payload_content_must_be_valid(cls, v):
+        if not isinstance(v, dict):
+            raise ValueError('Payload must be a dictionary')
+        valid_models = ['default', 'openai', 'claude', 'google']
+        if 'model' not in v:
+            raise ValueError('Payload must contain a model')
+        if v['model'] not in valid_models:
+            raise ValueError(f'Model must be one of {valid_models}')
+        if 'doc_id' not in v:
+            raise ValueError('Payload must contain a doc_id')
+        return v
+
+    @validator('username')
+    def username_must_be_email(cls, v):
+        if '@' not in v:
+            raise ValueError('Username must be an email address')
+        return v
+    
+    @validator('ttl')
+    def ttl_must_be_positive(cls, v):
+        if v < 1:
+            raise ValueError('TTL must be a positive integer')
+        if v > 10:
+            raise ValueError('TTL must be less than 10')
+        return v
--- routers/utility.py
+++ routers/utility.py
@@ -3,12 +3,16 @@ utility.py - Falcon API Routers for a collection of utility functions
 """
 from datetime import datetime
 import logging
+import os
+from dotenv import load_dotenv
 from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException, status
 from routers.api_version import APIVersion
 from util.utility import Utilities
 from models.realproperty import RealPropertyInfoRequest, RealPropertyInfoResponse
+from models.queue_request import QueueRequest
+from distributed_work_queue.workqueue import DistributedWorkQueue
 
-
+load_dotenv()
 API_VERSION = APIVersion(1, 0).to_str()
 ROUTE_PREFIX = '/util'
 LOGGER = logging.getLogger(f'falconapi{ROUTE_PREFIX}')
@@ -20,9 +24,16 @@ router = APIRouter(
     responses={404: {"description": "Not found in Utility Functions"}},
 )
 
+work_queue = DistributedWorkQueue(
+    os.getenv("WORK_QUEUE_HOST", 'localhost'),
+    int(os.getenv("WORK_QUEUE_PORT", "6379")),
+    int(os.getenv("WORK_QUEUE_DB", "0")),
+    os.getenv("WORK_QUEUE_NAME_CLASSIFY", 'classification_queue'),
+)
+
 # Retrieve property description and valuation data from ATTOM Data Solutions
 @router.get('/property', status_code=status.HTTP_200_OK, response_model=RealPropertyInfoResponse, summary='Get Property Details')
-#async def get_property_details(info_request: RealPropertyInfoRequest):
+# async def get_property_details(info_request: RealPropertyInfoRequest):
 async def get_property_details(address: str, city: str, state: str, zip_code: str):
     """
     Retrieve property description and valuation data from ATTOM Data Solutions
@@ -37,3 +48,16 @@ async def get_property_details(address: str, city: str, state: str, zip_code: st
         dict: Property details
     """
     return Utilities.get_property_details(address, city, state, zip_code)
+
+# Queue a request for processing
+@router.post('/queue', status_code=status.HTTP_201_CREATED, summary='Queue a Request')
+async def queue_request(request: QueueRequest, background_tasks: BackgroundTasks):
+    """
+    Queue a request for processing
+
+    Args:
+        request (QueueRequest): Request to be queued
+        background_tasks (BackgroundTasks): Background tasks to be executed
+    """
+    work_queue.enqueue_work(request)
+    return {"message": f"{request.task} task queued", "id": request.request_id}
