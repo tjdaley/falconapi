b28fcc2476071c97ce647b6566f45070b805d2ed
Tom Daley <tom@powerdaley.com>
2024-05-24 10:26:15 -0500

    Update handler.py

--- auth/handler.py
+++ auth/handler.py
@@ -32,7 +32,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl=f'api/{API_VERSION}{ROUTE_PREFIX}/
 class Token(BaseModel):
     access_token: str
     token_type: str
-    user_id: str = Field(alias='id', default='')
+    id: str = Field(serialization_alias='id', default='')
     twilio_factor_id: str = Field(default='')
 
     class Config:
