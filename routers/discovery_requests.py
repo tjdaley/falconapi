dccebef9a8caabb752fdb92dbb04c56836eb22ba
Thomas J. Daley, J.D. <tjd@powerdaley.com>
2024-08-18 10:54:40 -0500

    fix return type

--- routers/discovery_requests.py
+++ routers/discovery_requests.py
@@ -45,7 +45,7 @@ async def get_request_service(search_field: str, search_value: str, current_user
         'username': current_user.email
     }
     data: ServedRequests = DISCOVERY_REQUESTS_TABLE.get_request_service_list(**args)
-    return {'request_docs': data, 'success': True}
+    return {'request_docs': list(data.model_dump()), 'success': True}
 
 @router.get("/{id}", response_model=DiscoveryRequest, tags=["Discovery Requests"], summary="Get a specific discovery request")
 async def get_request(id: str, current_user: User = Depends(get_current_active_user)) -> DiscoveryRequest:
@@ -60,7 +60,7 @@ async def get_request(id: str, current_user: User = Depends(get_current_active_u
         DiscoveryRequest: The discovery request.
     """
     data: DiscoveryRequest = DISCOVERY_REQUESTS_TABLE.get_request(id, current_user.email)
-    return {'request': data, 'success': True}
+    return {'request': data.model_dump(), 'success': True}
 
 @router.post(
     '/',
