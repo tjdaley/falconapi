0715c752dc0d35f17872278463af30e986cb15a9
Thomas J. Daley, J.D. <tjd@powerdaley.com>
2024-08-18 21:11:34 -0500

    I can do this all night

--- database/discovery_requests.py
+++ database/discovery_requests.py
@@ -97,7 +97,7 @@ class DiscoveryRequestsTable(Database):
                     served_requests[key]['response_count'] += 1
 
         request_list = [ServedRequest(**served_request) for served_request in served_requests.values()]
-        return ServedRequests(request_list)
+        return ServedRequests(requests=request_list)
     
     def get_requests(self, served_by: str, served_date: str, request_type: str, client_id: str = None, username: str = None) -> ServedRequests:
         """
